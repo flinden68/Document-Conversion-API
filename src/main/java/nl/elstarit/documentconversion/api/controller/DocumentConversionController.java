efdc59c0804493c7340aeb9672170c07401245b2
Frank van der Linden <flinden68@elstarit.nl>
2017-11-25 19:43:49 +0100

    Made some changes to the endpoint and pushed a new version to Bluemix

--- src/main/java/nl/elstarit/documentconversion/api/controller/DocumentConversionController.java
+++ src/main/java/nl/elstarit/documentconversion/api/controller/DocumentConversionController.java
@@ -1,10 +1,11 @@
 package nl.elstarit.documentconversion.api.controller;
 
-import nl.elstarit.documentconversion.api.model.DocumentConversionRequest;
 import nl.elstarit.documentconversion.api.model.DocumentConversionResponse;
 import org.apache.commons.io.FileUtils;
 import org.apache.tika.metadata.Metadata;
 import org.apache.tika.parser.AutoDetectParser;
+import org.apache.tika.parser.ParseContext;
+import org.apache.tika.parser.pdf.PDFParser;
 import org.apache.tika.sax.BodyContentHandler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,8 +14,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletRequest;
+import java.io.BufferedInputStream;
 import java.io.InputStream;
 
 @Controller
@@ -24,16 +28,18 @@ public class DocumentConversionController {
     private static final Logger LOG = LoggerFactory.getLogger(DocumentConversionController.class);
 
     @RequestMapping("/convert-to-plain-text")
-    public ResponseEntity<DocumentConversionResponse> covertToPlainText(@RequestBody DocumentConversionRequest documentConversionRequest){
+    public ResponseEntity<DocumentConversionResponse> covertToPlainText(@RequestParam("file") MultipartFile file){
         BodyContentHandler handler = new BodyContentHandler();
 
         AutoDetectParser parser = new AutoDetectParser();
         Metadata metadata = new Metadata();
+        ParseContext context = new ParseContext();
         DocumentConversionResponse documentConversionResponse = new DocumentConversionResponse();
         try{
-            if(documentConversionRequest.getFile() != null ) {
-                InputStream stream = FileUtils.openInputStream(documentConversionRequest.getFile());
-                parser.parse(stream, handler, metadata);
+            LOG.info("FILE="+file.getName());
+            if(file != null ) {
+                InputStream stream = new BufferedInputStream(file.getInputStream());
+                parser.parse(stream, handler, metadata, context);
                 documentConversionResponse.setConvertedText(handler.toString());
             }else{
                 documentConversionResponse.setMessage("No File found in request");
